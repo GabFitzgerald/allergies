45f014930ce214b42bbc9afcca3d224c8a23f2a8
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-13 10:47:49 +1000

    style(allergies.cr): crystal tool format command run

--- src/allergies.cr
+++ src/allergies.cr
@@ -1,26 +1,26 @@
 # Please implement your solution to allergies in this file
 class Allergies
-    @@allergies = { "cats" => 128 ,
-        "pollen" => 64,
-        "chocolate" => 32,
-        "tomatoes" => 16,
-        "strawberries" => 8,
-        "shellfish" => 4,
-        "peanuts" => 2,
-        "eggs" => 1
-    }
+  @@allergies = {"cats"         => 128,
+                 "pollen"       => 64,
+                 "chocolate"    => 32,
+                 "tomatoes"     => 16,
+                 "strawberries" => 8,
+                 "shellfish"    => 4,
+                 "peanuts"      => 2,
+                 "eggs"         => 1,
+  }
 
-    def self.allergic_to(allergen : String, score : Int32) : Bool
-        list(score).includes?(allergen) ? true : false
-    end
+  def self.allergic_to(allergen : String, score : Int32) : Bool
+    list(score).includes?(allergen) ? true : false
+  end
 
-    def self.list(score : Int32) : Array
-        score %= 256
-        return @@allergies.map do |key, value|    
-            if score - value >= 0
-                score -= value
-                key
-            end
-        end.compact.reverse
-    end
+  def self.list(score : Int32) : Array
+    score %= 256
+    return @@allergies.map do |key, value|
+      if score - value >= 0
+        score -= value
+        key
+      end
+    end.compact.reverse
+  end
 end
